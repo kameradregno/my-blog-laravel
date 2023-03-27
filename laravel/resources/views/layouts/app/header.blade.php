6c179dc4a203c4172feb580bd1c45c03d3126562
kameradregno <regon0705@gmail.com>
2023-03-27 09:19:19 +0800

    blog

--- laravel/resources/views/layouts/app/header.blade.php
+++ laravel/resources/views/layouts/app/header.blade.php
@@ -6,15 +6,23 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 mx-4">
-          <li><a href="{{ url('posts/create') }}" class="nav-link px-2 text-white fs-4" style="font-family:'deathshead laser', monospace">Buat Post</a></li>
-          <li><a href="{{ url('posts/trash') }}" class="nav-link px-2 text-white fs-4" style="font-family:'deathshead laser', monospace">Riwayat Hapus</a></li>
-          <!-- <li><a href="#" class="nav-link px-2 text-white" style="font-family:'product sans', monospace">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-white" style="font-family:'product sans', monospace">About</a></li> -->
+        @if(Request::path() == 'login')
+        <li class="nav-item"><a href="#" class="nav-link text-muted">Buat Post</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-muted">Riwayat Hapus</a></li>
+        @else
+        <li class="nav-item"><a href="{{ url('posts/create') }}" class="nav-link px-2 text-white fs-4" style="font-family:'deathshead laser', monospace">Buat Post</a></li>
+        <li class="nav-item"><a href="{{ url('posts/trash') }}" class="nav-link px-2 text-white fs-4" style="font-family:'deathshead laser', monospace">Riwayat Hapus</a></li>
+        @endif
         </ul>
 
-        <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" style="font-family:'deathshead outline', monospace">
-        </form> -->
+        
+        @if(Request::path() != 'login')
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" style="font-family:'deathshead outline', monospace">
+        </form>
+        @endif
+
+
 
         <div class="text-end">
           <a href="{{ url("login") }}"  class="btn btn-outline-light me-2" style="font-family:'futura-book', monospace">Login</a>
