25402b2b451f9ed53c59ff0fb1530ba65ca4b228
kameradregno <regon0705@gmail.com>
2023-03-28 14:51:01 +0800

    blog

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 
@@ -56,6 +57,12 @@ class AuthController extends Controller
             'name' => 'min:3|max:10',
             'email' => 'email|unique:users',
             'password' => 'min:4|max:6|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
         ]);
 
 
